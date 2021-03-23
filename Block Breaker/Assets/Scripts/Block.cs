fd1e97664508c8e800e7dea649d07462f1b9b15c
Mr Janssen <jonathan.janssen@lifeschools.net>
2021-03-23 13:22:03 -0500

    Initial Commit

---
+++ Block Breaker/Assets/Scripts/Block.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour {
+
+    [SerializeField] AudioClip breakSound;
+    [SerializeField] GameObject blockSparklesVFX;
+    [SerializeField] float sparklesDestroy = 2.0f;
+    [SerializeField] Sprite[] hitSprites;
+    public GameObject multiBall;
+    public int multiBallCounter = 0;
+
+
+    // cached reference
+    Level level;
+
+    //state variables
+    [SerializeField] int timesHit; 
+
+
+    private void Start()
+    {
+        CountBreakableBlocks();
+
+    }
+
+    private void CountBreakableBlocks()
+    {
+        level = FindObjectOfType<Level>();
+        if (tag == "Breakable")
+        {
+            level.CountBlocks();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (tag == "Breakable")
+        {
+           HandleHit();
+        }
+    }
+
+    private void HandleHit()
+    {
+        timesHit++;
+        int maxHits = hitSprites.Length + 1;
+        if (timesHit >= maxHits)
+        {
+            DestroyBlock();
+        }
+        else
+        {
+            ShowNextHitSprite();
+        }
+    }
+
+    private void ShowNextHitSprite()
+    {
+        int spriteIndex = timesHit - 1;
+
+        if (hitSprites[spriteIndex] != null)
+        {
+            GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+        }
+        else
+        {
+            Debug.LogError("Block Sprite is missing from Array" + gameObject.name);
+        }
+    }
+
+    private void DestroyBlock()
+    {
+        PlayBlockDestroySFX();
+        Destroy(gameObject);
+        level.BlockDestroyed();
+        TriggerSparkles();
+        SpawnMultipleBalls();
+        
+    }
+
+    private void PlayBlockDestroySFX()
+    {
+        FindObjectOfType<GameStatus>().AddToScore();
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+    }
+
+    private void TriggerSparkles()
+    {
+
+        GameObject sparkles = Instantiate(blockSparklesVFX, transform.position, transform.rotation);
+        Destroy(sparkles, sparklesDestroy);
+
+    }
+    public void SpawnMultipleBalls()
+    {
+        if (multiBallCounter <= 0)
+        {
+
+            multiBallCounter++;
+            GameObject ball = Instantiate(multiBall, transform.position, Quaternion.identity) as GameObject;
+            multiBall.GetComponent<Rigidbody2D>().velocity = new Vector2(2.0f, 15.0f);
+
+        }
+
+    }
+}
